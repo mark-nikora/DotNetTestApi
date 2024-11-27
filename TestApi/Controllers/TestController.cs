bb11ad9e363159e5d516d7f7caf31fd9c6b0adaa
mark Nikora <mark@pinanyon.local>
2024-11-27 13:17:39 +1300

    wip - fixed tests

--- TestApi/Controllers/TestController.cs
+++ TestApi/Controllers/TestController.cs
@@ -1,5 +1,7 @@
-using System.Reflection.Metadata.Ecma335;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using TestApi.DTOs;
+using TestApi.Models;
 using TestApi.Repositories;
 using TestApi.Services;
 
@@ -7,17 +9,66 @@ namespace TestApi.Controllers
 {
     [Route("")]
     [ApiController]
-    public class TestController(ITestRepository testRepository, ITestService testService) : Controller
+    public class TestController(ITestRepository testRepository, ITestService testService, IMapper mapper) : Controller
     {
 
         private readonly ITestRepository _testRepository = testRepository;
         private readonly ITestService _testService = testService;
 
+        private readonly IMapper _mapper = mapper;
+
         [HttpGet]
         public async Task<IActionResult> GetById(int id)
         {
-            var testDto = await _testService.GetTestWithQuestions(id);
-            return testDto == null ? NotFound() : Ok(testDto);
+            try
+            {
+                var testDto = await _testService.GetTestWithQuestions(id);
+                return testDto == null ? NotFound() : Ok(testDto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] TestDetailsDto testDto)
+        {
+            var test = _mapper.Map<Test>(testDto);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                _testRepository.Update(test);
+                await _testRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TestDetailsDto testDto)
+        {
+            var test = _mapper.Map<Test>(testDto);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _testRepository.AddAsync(test);
+                await _testRepository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+            return NoContent();
         }
     }
 }
--- TestApi/Mappers/profile.cs
+++ TestApi/Mappers/profile.cs
@@ -11,6 +11,11 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
 
+        CreateMap<TestDetailsDto, Test>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+
         CreateMap<Question, QuestionDto>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
